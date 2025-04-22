bbf389ae6512866f1260901344616574a725a43a
Melvin Jose <josemelvin1403@gmail.com>
2025-04-22 18:30:29 +0200

    Update shortrna_Qc.sh

--- scripts/preprocessing/RNA_shortreads/shortrna_Qc.sh
+++ scripts/preprocessing/RNA_shortreads/shortrna_Qc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -A uppmax2025-3-3
 #SBATCH -p core
-#SBATCH -n 8
+#SBATCH -n 4
 #SBATCH -t 2-00:00:00
 #SBATCH -M snowy
 #SBATCH -J rna_shortreads_afterqc
@@ -17,7 +17,7 @@ module load MultiQC
 
 #qc for processed DNA short reads
 fastqc /proj/uppmax2025-3-3/nobackup/work/melvi/data/processed_reads/RNA_short_reads/*.fastq.gz \
--o /home/melvi/project/analyses_results/preprocess/rna_shortreads/after_qc --threads 8
+-o /home/melvi/project/analyses_results/preprocess/rna_shortreads/after_qc --threads 4
 
 #aggregating with multiqc
 multiqc /home/melvi/project/analyses_results/preprocess/rna_shortreads/after_qc \
