89fca5be79a049323d2c6b98a0ed7930c4f2144d
Melvin Jose <josemelvin1403@gmail.com>
2025-05-20 19:17:22 +0200

    Update flye.sh

--- scripts/assembly/init/flye.sh
+++ scripts/assembly/init/flye.sh
@@ -4,63 +4,26 @@
 #SBATCH -n 4
 #SBATCH -M snowy
 #SBATCH -t 48:00:00
-#SBATCH -J canu_assemble_only
+#SBATCH -J flye
 #SBATCH --mail-user=josemelvin1403@gmail.com
 #SBATCH --mail-type=ALL
 #SBATCH -o canu_corrected_%j.out
 #SBATCH -e canu_corrected_%j.err
 
-# Load modules
 module load bioinfo-tools
-module load canu
+module load Flye
 
-# Move to scratch space
-cd $SNIC_TMP
+flye --nano-corr /proj/uppmax2025-3-3/nobackup/work/melvi/data/processed_reads/DNA_long_reads/SRR24413066/SRR24413066.correctedReads.fasta.gz \
+     --out-dir /proj/uppmax2025-3-3/nobackup/work/melvi/assembly/processed/SRR24413066 \
+     --genome-size 9.6m \
+     --threads 4
 
-# Copy corrected reads and decompress them
-cp /proj/uppmax2025-3-3/nobackup/work/melvi/data/processed_reads/DNA_long_reads/SRR24413066/SRR24413066.correctedReads.fasta.gz ./corrected.fasta.gz
-gunzip corrected.fasta.gz  # decompress to corrected.fasta
+flye --nano-corr /proj/uppmax2025-3-3/nobackup/work/melvi/data/processed_reads/DNA_long_reads/SRR24413066/SRR24413072.correctedReads.fasta.gz \
+     --out-dir /proj/uppmax2025-3-3/nobackup/work/melvi/assembly/processed/SRR24413072 \
+     --genome-size 9.6m \
+     --threads 4
 
-# Run Canu in assembly-only mode
-canu \
- -p canu_SRR24413066 \
- -d canu_corrected_out \
- genomeSize=9.6m \
- -nanopore-corrected corrected.fasta \
- useGrid=false \
- maxThreads=4
-
-# Copy output back
-cp -r canu_corrected_out /proj/uppmax2025-3-3/nobackup/work/melvi/assembly/canu_assembly/SRR24413066
-
-# Copy corrected reads and decompress them
-cp /proj/uppmax2025-3-3/nobackup/work/melvi/data/processed_reads/DNA_long_reads/SRR24413072/SRR24413072.correctedReads.fasta.gz ./corrected.fasta.gz
-gunzip corrected.fasta.gz  # decompress to corrected.fasta
-
-# Run Canu in assembly-only mode
-canu \
- -p canu_SRR24413072 \
- -d canu_corrected_out \
- genomeSize=9.6m \
- -nanopore-corrected corrected.fasta \
- useGrid=false \
- maxThreads=4
-
-# Copy output back
-cp -r canu_corrected_out /proj/uppmax2025-3-3/nobackup/work/melvi/assembly/canu_assembly/SRR24413072
-
-# Copy corrected reads and decompress them
-cp /proj/uppmax2025-3-3/nobackup/work/melvi/data/processed_reads/DNA_long_reads/SRR24413081/SRR24413081.correctedReads.fasta.gz ./corrected.fasta.gz
-gunzip corrected.fasta.gz  # decompress to corrected.fasta
-
-# Run Canu in assembly-only mode
-canu \
- -p canu_SRR24413081 \
- -d canu_corrected_out \
- genomeSize=9.6m \
- -nanopore-corrected corrected.fasta \
- useGrid=false \
- maxThreads=4
-
-# Copy output back
-cp -r canu_corrected_out /proj/uppmax2025-3-3/nobackup/work/melvi/assembly/canu_assembly/SRR24413081
+flye --nano-corr /proj/uppmax2025-3-3/nobackup/work/melvi/data/processed_reads/DNA_long_reads/SRR24413066/SRR24413081.correctedReads.fasta.gz \
+     --out-dir /proj/uppmax2025-3-3/nobackup/work/melvi/assembly/processed/SRR24413081 \
+     --genome-size 9.6m \
+     --threads 4
