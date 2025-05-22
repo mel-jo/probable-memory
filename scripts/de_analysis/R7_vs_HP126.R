14bb34ca2485acf823f617e8ec36f8a1d7170de9
Melvin Jose <josemelvin1403@gmail.com>
2025-05-22 20:33:50 +0200

    Update R7_vs_HP126.R

--- scripts/de_analysis/R7_vs_HP126.R
+++ scripts/de_analysis/R7_vs_HP126.R
@@ -1,11 +1,10 @@
 # Install & load
 if (!requireNamespace("BiocManager", quietly=TRUE)) install.packages("BiocManager")
-pkgs <- c("DESeq2","edgeR","apeglm","tidyverse","pheatmap")
+pkgs <- c("DESeq2","edgeR","apeglm","tidyverse","pheatmap", "ggrepel")
 BiocManager::install(pkgs, ask=FALSE, update=FALSE)
 lapply(pkgs, library, character.only=TRUE)
 
 # Read data & define samples
-
 counts <- read.delim(
   "C:/Users/josem/OneDrive/Documents/Genome Analysis/de_analysis/bwa_featurecounts_matrix.txt",
   comment.char="#", row.names=1, check.names=FALSE
@@ -19,6 +18,7 @@ sample_ids <- c(
   "SRR24516459","SRR24516460","SRR24516461",
   "SRR24516462","SRR24516463","SRR24516464"
 )
+
 counts_sub <- counts[, sample_ids]
 
 sample_info <- data.frame(
@@ -26,22 +26,19 @@ sample_info <- data.frame(
   condition = rep(c("HP126","R7"), each=3)
 )
 
-sample_info$condition <- factor(sample_info$condition,
-                                levels = c("R7","HP126"))
+sample_info$condition <- factor(sample_info$condition, levels = c("R7","HP126"))
 
 
 # Unfiltered
 dds_unf <- DESeqDataSetFromMatrix(counts_sub, sample_info, ~condition)
 dds_unf <- DESeq(dds_unf)
+res_unf_raw <- results(dds_unf) 
 
-res_unf_raw <- results(dds_unf)  
-# by default uses coef = "condition_HP126_vs_R7"
 res_unf_shr <- lfcShrink(
   dds_unf,
   coef = "condition_HP126_vs_R7",
   type = "apeglm"
 )
-
 
 # filter at padj < .05 & |LFC|>=1
 sig_unf <- subset(res_unf_shr, padj < .05 & abs(log2FoldChange) >= 1)
@@ -49,8 +46,7 @@ sig_unf <- subset(res_unf_shr, padj < .05 & abs(log2FoldChange) >= 1)
 cat("[Unfiltered] tested:", nrow(res_unf_shr),
     "| significant:", nrow(sig_unf), "\n")
 
-
-# 3. CPM-filtered DESeq2
+# CPM-filtered DESeq2
 
 dge <- DGEList(counts_sub)
 keep <- rowSums(cpm(dge) > 1) >= 3
@@ -60,7 +56,6 @@ dds_filt <- DESeqDataSetFromMatrix(counts_filt, sample_info, ~condition)
 dds_filt <- DESeq(dds_filt)
 
 res_filt_raw <- results(dds_filt)  
-# again uses coef = "condition_HP126_vs_R7"
 res_filt_shr <- lfcShrink(
   dds_filt,
   coef = "condition_HP126_vs_R7",
@@ -77,13 +72,7 @@ write.csv(
   row.names = FALSE
 )
 
-
-library(DESeq2)
-library(tidyverse)
-library(pheatmap)
-library(ggrepel)
-
-
+# Generating the required plots
 vsd <- vst(dds_filt, blind=FALSE)
 
 # extract matrix & sample info
@@ -103,7 +92,6 @@ ggplot(pcaData, aes(PC1, PC2, color=condition)) +
 
 
 # Heatmap of top 30 DEGs
-
 # select top 30 by padj
 top30 <- res_filt_shr %>%
   as.data.frame() %>%
